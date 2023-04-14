bb79c7143b29ea327900d8694bbe6399ae2ab641
Edmund Debrah <edmundking05@gmail.com>
2023-04-14 17:59:34 +0000

    create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { useState, useEffect } from 'react'
+import React from "react"
+import Header from "./Header"
+import ConversationPanel from "./ConversationPanel"
+import PromptBar from "./PromptBar"
 import './App.css'
 
 function App() {
   return (
     <div>
+      <Header />
+      <ConversationPanel />
+      <PromptBar />
     </div>
   )
 }
