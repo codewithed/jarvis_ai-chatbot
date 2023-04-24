8f1155da59b7ff24cb17d2c26054eb25d58e48f8
Edmund Debrah <edmundking05@gmail.com>
2023-04-24 13:42:40 +0000

    update mmessage styling

--- src/Message.tsx
+++ src/Message.tsx
@@ -4,7 +4,7 @@ import UserAvatar from './assets/user-avatar.png'
 
 export default function Response(props :any){
     return (
-        <div className={props.bgColor}>
+        <div className={`flex text-[whitesmoke] ${props.bgColor}`}>
             <img src={props.role === 'question' ? UserAvatar : ChatAvatar} className=""></img>
             <p className="question-text">{props.content}</p>
         </div>
