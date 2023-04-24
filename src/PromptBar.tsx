d56d386e91c05dd53391d4169b29c0885c291de6
Edmund Debrah <edmundking05@gmail.com>
2023-04-24 13:43:39 +0000

    update promptbar to use prevMessages

--- src/PromptBar.tsx
+++ src/PromptBar.tsx
@@ -5,12 +5,12 @@ export default function PromptBar(props: any) {
         <div className="flex-col p-4">
             <div className='w-[100%] flex justify-center'>
             <div className='flex items-center justify-center gap-3 bg-[#444654] rounded-lg drop-shadow-2xl p-2 w-[fit-content]'>
-            <textarea className='min-w-[70vw] h-[50px] bg-transparent text-[whitesmoke] overflow-y-hidden' value={props.value} 
-            onChange={(e) => {props.setCurrentMessage(e.target.value); } }></textarea>
+            <textarea className='min-w-[70vw] h-[50px] bg-transparent text-[whitesmoke] overflow-y-hidden'
+            value={props.value}
+            onChange={(e) => props.setCurrentMessage(e.target.value) }></textarea>
             <button onClick={() => {
-                props.setMessages([...props.messages, {role: "user", content: props.value}]);
-                console.log(props.messages)
-                //props.sendMessage
+                props.setMessages((prevMessages: any) => [...prevMessages, {role: "user", content: props.value}]);
+                props.sendToChatGPT
             }}>
                 <img src={Send} className="h-[20px]"></img>
             </button>
