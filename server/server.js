6f9e9e19f996e6a9df3ee75e21e1fbdab670dae8
Edmund Debrah <edmundking05@gmail.com>
2023-04-27 20:41:21 +0000

    set up api proxy

--- server/server.js
+++ server/server.js
@@ -6,6 +6,29 @@ const app = express();
 const port = process.env.PORT || 3000;
 const key = process.env.API_KEY;
 
+app.post('/api', (req, res) => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: req.body.messages,
+      max_tokens: 1024,
+    }),
+  };
+  // process the response
+  fetch('https://api.openai.com/v1/completions', requestOptions)
+    .then((response) => {
+    // handle response
+      res.send(response.json());
+    })
+    .catch((error) => console.error(error));
+});
+
+// start the server
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
