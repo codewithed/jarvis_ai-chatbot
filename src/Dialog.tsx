56741ff83398524057a6bd6636386febe7436e8a
Edmund Debrah <edmundking05@gmail.com>
2023-09-04 22:33:32 +0000

    fix message rendering bug

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -11,14 +11,17 @@ interface DialogProps {
 }
 
 export default function Dialog({messages} :DialogProps){
+    const messageComponents = messages.map((message: Message) => {
+    return(
+    <Message 
+    key = {crypto.randomUUID()}
+    role={message.role}
+    content={message.content} 
+    bgColor={message.role === 'user' ? "bg-[#343541]" : "bg-[#444654]"}/>
+ )})
     return (
         <div className="flex-grow  min-h-[76vh] bg-[#262627] overflow-auto gap-1">
-            {Array.from(messages).map((message: Message) => message.content !== "" && <Message 
-                key = {crypto.randomUUID()}
-                role={message.role}
-                content={message.content} 
-                bgColor={message.role === 'user' ? "bg-[#343541]" : "bg-[#444654]"}/>
-             )}
+            {messageComponents}
         </div>
     )
 }
